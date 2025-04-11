dbfffd48a86174ecd53b1dd15234635a9fdfc2da
porkboi <christian.ang.kx@gmail.com>
2025-04-11 00:49:17 -0400

    add checkin

--- components/party-status-indicator.tsx
+++ components/party-status-indicator.tsx
@@ -24,19 +24,19 @@ export function PartyStatusIndicator({ checkedInCount, maxCapacity }: PartyStatu
     if (percentage < 33.33) {
       setStatus({
         color: "green",
-        text: "It's dead",
+        text: "    It's dead",
         className: "pulse-light-green",
       })
     } else if (percentage < 66.66) {
       setStatus({
         color: "yellow",
-        text: "Lively",
+        text: "    Lively",
         className: "pulse-light-yellow",
       })
     } else {
       setStatus({
         color: "red",
-        text: "WOOOO",
+        text: "    WOOOO",
         className: "pulse-light-red",
       })
     }
@@ -45,7 +45,7 @@ export function PartyStatusIndicator({ checkedInCount, maxCapacity }: PartyStatu
   return (
     <div className="flex items-center">
         <span className={`h-4 w-4 rounded-full bg-${status.color}-500 animate-ping`}></span>
-        <span className="font-medium">{    status.text}</span>
+        <span className="font-medium">{status.text}</span>
     </div>
   )
 }
