34f5e07bb2b90764510d3bfebbe52b50b5f81f5f
porkboi <christian.ang.kx@gmail.com>
2025-04-25 18:41:06 -0400

    page

--- components/word-art.tsx
+++ components/word-art.tsx
@@ -45,53 +45,33 @@ export const ProWordArt = () => {
         onMouseMove={handleMouseMove}
         onMouseEnter={() => setIsHovering(true)}
         onMouseLeave={() => setIsHovering(false)}
-        className="relative flex items-center justify-center h-[300px] overflow-hidden"
+        className="relative flex items-center justify-center"
       >
-        {/* Masked light effect using text shape */}
-        <div
-          className="absolute z-10 h-full w-full pointer-events-none"
-          style={{
-            WebkitMaskImage: `text`,
-            WebkitMaskRepeat: "no-repeat",
-            WebkitMaskPosition: "center",
-            WebkitMaskSize: "contain",
-            maskComposite: "intersect", // Some browsers
-          }}
-        >
-          {isHovering && (
-            <div
-              className="absolute h-[200px] w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,_rgba(255,255,255,0.7)_0%,_transparent_70%)]"
-              style={{
-                left: `${position.x}px`,
-                top: `${position.y}px`,
-              }}
-            />
-          )}
-        </div>
+        {isHovering && (
+          <div
+            className="pointer-events-none absolute z-10 h-[200px] w-[200px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[radial-gradient(circle,_rgba(255,255,255,0.7)_0%,_transparent_70%)]"
+            style={{
+              left: `${position.x}px`,
+              top: `${position.y}px`,
+              mixBlendMode: "normal", // remove light blending with background
+            }}
+          />
+        )}
   
-        {/* Actual text as mask */}
-        <h1
-          className="z-20 text-[7rem] font-extrabold text-white opacity-0 pointer-events-none select-none"
-          style={{
-            WebkitMaskImage: "text",
-            WebkitMaskRepeat: "no-repeat",
-            WebkitMaskPosition: "center",
-            WebkitMaskSize: "contain",
-          }}
+        <motion.h1
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.6 }}
+          className="relative z-20 text-[7rem] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-700 cursor-pointer"
         >
-          Lite
-        </h1>
-  
-        {/* Color Text */}
-        <h1 className="relative z-20 text-[7rem] font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-700">
-          <span className="absolute left-0 top-0 text-pink-500 drop-shadow-[0_0_5px_rgba(74,20,140,0.8)]">
+          <span className="absolute left-0 top-0 text-[7rem] font-extrabold text-pink-500 drop-shadow-[0_0_5px_rgba(74,20,140,0.8)]">
             Lite
           </span>
-          <span className="absolute left-0 top-0 text-purple-700 drop-shadow-[0_0_15px_rgba(128,0,128,0.6)]">
+          <span className="absolute left-0 top-0 text-[7rem] font-extrabold text-purple-700 drop-shadow-[0_0_15px_rgba(128,0,128,0.6)]">
             Lite
           </span>
           <span className="relative z-20">Lite</span>
-        </h1>
+        </motion.h1>
       </div>
     )
   }
