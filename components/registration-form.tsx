81a9e92e2ea45f921848bf6bfe9a8f96aa26c1bf
porkboi <christian.ang.kx@gmail.com>
2025-09-23 00:52:14 -0400

    Bug: AndrewIDs can be 2 chars long

--- components/registration-form.tsx
+++ components/registration-form.tsx
@@ -70,8 +70,8 @@ export function RegistrationForm({ party, partySlug }: RegistrationFormProps) {
               message: "You must be at least 18 years old.",
             }
           ),
-        andrewID: z.string().min(3, {
-          message: "Andrew ID must be at least 3 characters.",
+        andrewID: z.string().min(2, {
+          message: "Andrew ID must be at least 2 characters.",
         }),
         paymentMethod:
           currentTierPrice > 0
